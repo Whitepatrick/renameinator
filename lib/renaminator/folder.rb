6bc0b8cd8d39fbb15ae254b52272f9fb9f0eaa9d
Patrick White <whitep@vcu.edu>
2015-10-11 13:27:20 -0400

    taking a break

--- lib/renaminator/folder.rb
+++ lib/renaminator/folder.rb
@@ -1,11 +1,19 @@
 # encoding: utf-8
 module Renaminator
   class Folder
+    attr_reader :base_folder
+    attr_accessor :media_dirs
+    def initialize(base_folder = "media/")
+      @base_folder = base_folder
+      @media_dirs = ["tv/", "movies/", "music/"]
+    end
     # gets local path and file info of folder
     # decide if destination is movie, tv, or music
     # query db for file info
     # create media object (factory?)
-    def get_folder_info
+    def get_folder_hash
+      dirs = Dir.glob("media/**/*")
+      Hash[*dirs]
     end
     def decide_media_type
     end
--- spec/folder_spec.rb
+++ spec/folder_spec.rb
@@ -1,11 +1,42 @@
 # encoding: utf-8
 require './spec/spec_helper'
 
-describe Renaminator::Folder do
+RSpec.describe Renaminator::Folder do
 
-  describe '.get_folder_info' do
-    describe '#folder?' do
+  describe 'Folder properties' do
+    it "defaults base folder = media/" do
+      folder = Renaminator::Folder.new
+      expect(folder.base_folder).to eq("media/")
+    end
+    it "can set base folder" do
+      folder = Renaminator::Folder.new("test_media/")
+      expect(folder.base_folder).to eq("test_media/")
+    end
+    it 'checks media folders available' do
+      folder = Renaminator::Folder.new
+      expect(folder.media_dirs).to eq(["tv/", "movies/", "music/"])
+    end
+  end
+
+  describe '.get_folder_hash' do
+    it 'creates a hash of folder contents' do
+      folder = Renaminator::Folder.new
+       dirs = Dir.glob("media/**/*")
+      expect(folder.get_folder_hash).to eq(Hash[*dirs])
+    end
+  end
+
+  describe '.make_info_object' do
+    it 'packages folder info' do
+      folder = Renaminator::Folder.new
       
     end
   end
+
+  describe '.query_db_api' do
+    it 'sets db uri from folder info'
+    it 'query''s using name from folder info'
+  end
+
+
 end

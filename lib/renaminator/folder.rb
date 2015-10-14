5ac8b741f566b330aa6e3a5e8d3b358550acb4a7
Patrick White <whitep@vcu.edu>
2015-10-13 20:04:03 -0400

    maybe ONE day I'll finish the first damn class with tests

--- lib/renaminator/folder.rb
+++ lib/renaminator/folder.rb
@@ -1,18 +1,21 @@
 # encoding: utf-8
 module Renaminator
   class Folder
-    attr_reader  :base_folder
-    attr_accessor :folder_hash
-    def initialize(base_folder = "spec/media",
-                   folder_hash = Hash.new{ "folders" })
-      @base_folder = base_folder
-      @folder_hash = folder_hash
+
+    attr_accessor :folder_hash, :file_hash
+    def initialize
+      @folder_hash = Hash.new
+      @file_hash = Hash.new
     end
-    def get_folder_hash
-      Find.find(base_folder) do |path|
-        folder_hash[:folders] = path if path =~ /.*\.file$/
-      end
+
+    def get_hash(dir, hash_type)
+      Dir.entries("#{dir}").each { |d| "#{hash_type}"["#{d}"] = "" }
     end
+
+    def remove_working_dirs(arr)
+      arr - [".", "..", "*.nfo", "*.txt"]
+    end
+
     def query_db_api
     end
     def make_info_object
--- spec/folder_spec.rb
+++ spec/folder_spec.rb
@@ -1,34 +1,32 @@
 # encoding: utf-8
 require './spec/spec_helper'
-
-RSpec.describe Renaminator::Folder do
-
-  describe 'Folder properties' do
-    it "defaults base folder = media/" do
-      folder = Renaminator::Folder.new
-      expect(folder.base_folder).to eq("spec/media")
+module Renaminator
+  RSpec.describe Folder do
+    
+    describe '.get_hash' do
+      it 'gets an array containing path names' do
+        folder = Folder.new
+        folder.get_hash("spec/media", "folder_hash")
+      end
     end
-    it "can set base folder" do
-      folder = Renaminator::Folder.new("test_media")
-      expect(folder.base_folder).to eq("test_media")
+
+    describe '.remove_working_dirs' do
+      it 'removes current/parent dirs from folder hash' do
+        folder = Folder.new
+        no_working_dirs = folder.remove_working_dirs(["folder_a", "folder_b", ".", ".."])
+        expect(no_working_dirs).to eq(["folder_a", "folder_b"])
+      end
     end
-  end
 
-  describe '.get_folder_hash' do
-    it 'gets hash with folders and files' do
-      folder = Renaminator::Folder.new
-      p folder.get_folder_hash.class
+    describe '.make_info_object' do
+      it 'packages folder info'
     end
+
+    describe '.query_db_api' do
+      it 'sets db uri from folder info'
+      it 'query''s using name from folder info'
+    end
+
+
   end
-
-  describe '.make_info_object' do
-    it 'packages folder info'
-  end
-
-  describe '.query_db_api' do
-    it 'sets db uri from folder info'
-    it 'query''s using name from folder info'
-  end
-
-
 end

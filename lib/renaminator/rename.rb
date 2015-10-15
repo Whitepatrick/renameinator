80e6caec2830416f3910e1b613bcb47cb4aadeb2
Patrick White <whitep@vcu.edu>
2015-10-15 19:39:23 -0400

    working renamer

--- lib/renaminator/rename.rb
+++ lib/renaminator/rename.rb
@@ -1,15 +1,35 @@
 # encoding: utf-8
 module Renaminator
   class Rename
-    # takes object with TV or MOVIE params
-    # sets file name variables from object params
-    # removes junk (.nfo, .txt, etc.)
-    # renames files
-    # store info into DB (optional)
-    def remove_junk(folder)
-      folder
+    attr_reader :series, :season, :episode
+    attr_accessor :destination#, :file_names
+    def initialize(destination)
+      @destination = destination
+      #@file_names = Dir.glob("media/#{destination}/*")
+
+      @series = "Show_show"
+      @season = "2"
+      @episode = "13"
     end
-    def rename_files
+
+    def rename
+      puts "Renaming TV files..."
+
+      folder_path = "media/#{destination}"
+      Dir.glob("#{folder_path}/*").sort.each do |f|
+        filename = "Edit_#{File.basename(f, File.extname(f))}_S0E01"
+        File.rename(f, "#{folder_path}/#{filename}")
+        puts "Now renaming #{f} to: #{filename}"
+      end
+    puts "Renaming complete."
     end
   end
 end
+
+ren = Renaminator::Rename.new("cool_show")
+ren.rename
+=begin
+#filename = File.basename(f, File.extname(f))
+file_name = "#{series}_S#{season}E#{episode}#{File.extname(f)}"
+File.rename(f, "#{folder_path}/#{file_name}")
+=end

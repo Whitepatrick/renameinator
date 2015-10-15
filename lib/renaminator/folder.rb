4c9927a04251c224701b4291c721f9798f2187cd
Patrick White <whitep@vcu.edu>
2015-10-15 08:06:31 -0400

    changes before work

--- lib/renaminator/folder.rb
+++ lib/renaminator/folder.rb
@@ -2,23 +2,31 @@
 module Renaminator
   class Folder
 
+    attr_reader :base_path
     attr_accessor :folder_hash, :file_hash
-    def initialize
+    def initialize(base_path = "spec/media")
+      @base_path = base_path
       @folder_hash = Hash.new
       @file_hash = Hash.new
     end
 
-    def get_hash(dir, hash_type)
-      Dir.entries("#{dir}").each { |d| "#{hash_type}"["#{d}"] = "" }
+    def get_folder_hash(dir)
+      folder_hash["#{dir}"] = Dir.entries("#{dir}")
+    end
+
+    def get_file_hash(folder_array)
+      Hash[*folder_array.flatten]
+      #folder_hash["#{folder}"] = Dir.entries("#{base_path}/#{folder}")
     end
 
     def remove_working_dirs(arr)
       arr - [".", "..", "*.nfo", "*.txt"]
     end
 
-    def query_db_api
+    def hash_merge
     end
-    def make_info_object
+
+    def query_db_api
     end
   end
 end
--- spec/folder_spec.rb
+++ spec/folder_spec.rb
@@ -2,11 +2,20 @@
 require './spec/spec_helper'
 module Renaminator
   RSpec.describe Folder do
-    
-    describe '.get_hash' do
+
+    describe '.get_folder_hash' do
       it 'gets an array containing path names' do
         folder = Folder.new
-        folder.get_hash("spec/media", "folder_hash")
+        folders =
+        expect(folder.get_folder_hash("spec/media")).to eq(Dir.entries("spec/media"))
+      end
+    end
+
+    describe '.get_file_hash' do
+      it 'gets a hash of folders with files' do
+        folder = Folder.new
+        folder_hash = folder.get_folder_hash("spec/media")
+        folder.get_file_hash(folder_hash.folder.remove_working_dirs)
       end
     end
 
@@ -16,10 +25,18 @@ module Renaminator
         no_working_dirs = folder.remove_working_dirs(["folder_a", "folder_b", ".", ".."])
         expect(no_working_dirs).to eq(["folder_a", "folder_b"])
       end
+      it 'removes working dirs from get hash object' do
+        folder = Folder.new
+        no_working_dirs = folder.remove_working_dirs(folder.get_folder_hash("spec/media"))
+        expect(no_working_dirs).to eq(["cool_show", "other_cool_show"])
+      end
     end
 
-    describe '.make_info_object' do
-      it 'packages folder info'
+    describe '.hash_merge' do
+      it 'merges folder and file hashes together' do
+        folder = Folder.new
+        folder.hash_merge
+      end
     end
 
     describe '.query_db_api' do

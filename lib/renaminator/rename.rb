adee3dcb266aed5506270a50728abd69dcead75c
Patrick White <whitep@vcu.edu>
2015-10-18 14:26:07 -0400

    some work on rename

--- lib/renaminator/rename.rb
+++ lib/renaminator/rename.rb
@@ -1,25 +1,16 @@
 # encoding: utf-8
 module Renaminator
   class Rename
-
-    attr_reader :folder_path
-    attr_accessor :destination
-    def initialize(destination)
-      @destination = destination
-      @folder_path = "media/#{destination}"
-    end
-
-    def rename
-      puts "Renaming #{destination} files..."
-      Dir.glob("#{folder_path}/*").sort.each do |f|
-        filename = "Edit_#{File.basename(f, File.extname(f))}_S0E01"
-        File.rename(f, "#{folder_path}/#{filename}")
-        puts "Now renaming #{f} to: #{filename}"
-      end
-    puts "Renaming complete."
+    def rename(files)
+      puts "Renaming files..."
     end
   end
 end
-
-#ren = Renaminator::Rename.new("cool_show")
-#ren.rename
+=begin
+files.sort.each do |f|
+  filename = "Edit_#{File.basename(f, File.extname(f))}_S0E01"
+  File.rename("media/#{f}", "media/#{filename}")
+  puts "Now renaming #{f} to: #{filename}"
+end
+puts "Renaming complete."
+=end
--- spec/rename_spec.rb
+++ spec/rename_spec.rb
@@ -1,11 +1,12 @@
 # encoding: utf-8
 require './spec/spec_helper'
-
-RSpec.describe Renaminator::Rename do
-  describe '.remove_junk' do
-    it "gets folder object from Folder"
-  end
-  describe ".rename_files" do
-    it "should take an hash object with array values"
+module Renaminator
+  RSpec.describe Rename do
+    describe '.rename' do
+      it "renames an array of files"
+    end
+    describe ".rename_files" do
+      it "should take an hash object with array values"
+    end
   end
 end
